22c37deaba7a532079ed046ef390260b93d69bd5
jpowell <jpowell@jpowpow.attlocal.net>
2019-02-23 13:48:23 -0600

    Memory class

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -235,27 +235,3 @@ public class Memory extends Application {
         Application.launch(args);
     }
 }
-//Class to create the DeckOfCards object
-class DeckOfCards {
-    //Class variable
-    private ArrayList<Integer> deck = new ArrayList<>();
-    //Constructor
-    DeckOfCards() {
-        //Add 52 cards and their card values in the deck
-        for (int i = 0; i < 52; i++) {
-                deck.add(i);
-            }
-            //Shuffle the deck
-            Collections.shuffle(deck);
-        
-    }
-    //Method to return the card value
-    public int get(int index) {
-        return deck.get(index);
-    }
-    //Method to return the size of the deck
-    public int size() {
-        return deck.size();
-    }
-    
-}
